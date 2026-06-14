f1b7a1e8044f876684b7bcca9b18a7b0ec249385
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2026-06-14 13:29:34 +0530

    https://leetcode.com/problems/isomorphic-strings/description/

---
+++ 14. Isomorphic strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        st = {}
+        ts = {}
+        if len(s)!=len(t):
+            return False
+        for i, j in zip(s,t):
+            if i in st:
+                if st[i]!=j:
+                    return False
+            else:
+                st[i]=j
+            if j in ts:
+                if ts[j] != i:
+                    return False
+            else:
+                ts[j]= i
+        return True
